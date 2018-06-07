a6fa1dfc04f80e66404476ad6b354f1b6bbd0dbd
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-08 01:22:36 +0900

    add regions to stream.

--- streamborne/stream.py
+++ streamborne/stream.py
@@ -8,11 +8,14 @@ class Stream(Generic[T]):
     def __init__(self, data: Iterator[T]) -> None:
         self.data = data
 
+    # region intermediate operations
     def map(self, func: Callable[[T], U]) -> 'Stream[U]':
         return Stream(map(func, self.data))
 
+    # endregion
+    # region terminal operation
     def as_list(self) -> List[T]:
         return list(self.data)
-
+    # endregion
 def from_list(data: Iterator[T]) -> Stream[T]:
     return Stream(data)
