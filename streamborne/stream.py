c44b17d9c245dba3d5f73ffc5805956e01c6166c
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-08 01:39:34 +0900

    add "closed" property and private functions to stream.

--- streamborne/stream.py
+++ streamborne/stream.py
@@ -1,21 +1,38 @@
 import itertools
-from typing import Callable, Generic, Iterator, List, TypeVar
+from typing import Callable, Generic, Iterable, Iterator, List, TypeVar
 
 T = TypeVar('T')
 U = TypeVar('U')
 
 class Stream(Generic[T]):
+
     def __init__(self, data: Iterator[T]) -> None:
         self.data = data
+        self.closed = False
 
     # region intermediate operations
     def map(self, func: Callable[[T], U]) -> 'Stream[U]':
-        return Stream(map(func, self.data))
+        return self.next(map(func, self.data))
 
     # endregion
     # region terminal operation
     def as_list(self) -> List[T]:
         return list(self.data)
+    # endregion
+    # region private functions
+    def next(self, iterable: Iterable[U]) -> Stream[U]:
+        if (self.closed):
+            raise IOError
+        else:
+            self.closed = True
+            return Stream(iterable)
+
+    def terminate(self, result_supplier):
+        if (self.closed):
+            raise IOError
+        else:
+            self.closed = True
+            return result_supplier
     # endregion
 def from_list(data: Iterator[T]) -> Stream[T]:
     return Stream(data)
