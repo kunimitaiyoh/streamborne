3d28a9dc1b9b0130230cefd5864c30fc2c5e3edb
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-07 16:46:46 +0900

    make Option.is_present property.

--- streamborne/option.py
+++ streamborne/option.py
@@ -6,5 +6,6 @@ class Option(Generic[T]):
     def __init__(self, payload: T) -> None:
         self.payload = payload
 
+    @property
     def is_present(self) -> bool:
         return self.payload is not None
