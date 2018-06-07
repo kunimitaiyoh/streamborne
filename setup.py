b78841c02c0729072a2f7897b51479a730fab36d
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-07 15:58:19 +0900

    edit setup.py. introduce mypy.

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ def get_readme():
     with open(path.dirname(path.abspath(__file__)) + '/README.md', encoding='utf8') as f:
         return f.read()
 
+def get_requirements(name):
+    with open(path.dirname(path.abspath(__file__)) + '/' + name) as f:
+        return [name.rstrip() for entry in f.readline()]
+
 setup(
     name='streamborne',
     version='0.0.1',
@@ -15,8 +19,13 @@ setup(
     description='A completely statically typed superset of itertools that provides a fluent API for data operations.',
     long_description=get_readme(),
     packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    tests_require=get_requirements('test-requirements.txt'),
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3 :: Only'
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
     ]
 )
