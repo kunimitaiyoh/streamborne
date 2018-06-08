265117192152337e4ff4c8f2b6f830115d28c470
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-08 11:31:41 +0900

    annotate types to stream.

--- streamborne/stream.py
+++ streamborne/stream.py
@@ -1,38 +1,37 @@
 import itertools
-from typing import Callable, Generic, Iterable, Iterator, List, TypeVar
+from typing import Callable, Generic, Iterable, List, TypeVar
 
 T = TypeVar('T')
 U = TypeVar('U')
 
 class Stream(Generic[T]):
-
-    def __init__(self, data: Iterator[T]) -> None:
+    def __init__(self, data: Iterable[T]) -> None:
         self.data = data
         self.closed = False
 
     # region intermediate operations
     def map(self, func: Callable[[T], U]) -> 'Stream[U]':
-        return self.next(map(func, self.data))
+        return self.next(lambda: map(func, self.data))
 
     # endregion
     # region terminal operation
     def as_list(self) -> List[T]:
-        return list(self.data)
+        return self.terminate(lambda: list(self.data))
     # endregion
     # region private functions
-    def next(self, iterable: Iterable[U]) -> Stream[U]:
+    def next(self, iterable_supplier: Callable[[], Iterable[U]]) -> 'Stream[U]':
         if (self.closed):
             raise IOError
         else:
             self.closed = True
-            return Stream(iterable)
+            return Stream(iterable_supplier())
 
-    def terminate(self, result_supplier):
+    def terminate(self, result_supplier: Callable[[], U]) -> U:
         if (self.closed):
             raise IOError
         else:
             self.closed = True
-            return result_supplier
+            return result_supplier()
     # endregion
-def from_list(data: Iterator[T]) -> Stream[T]:
+def from_list(data: Iterable[T]) -> Stream[T]:
     return Stream(data)
