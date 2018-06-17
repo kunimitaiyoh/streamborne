fb1b0cfa28d4ce690b5db17c74a05c9d7a0fa29c
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-17 19:09:25 +0900

    add empty test methods for class Option.

--- test/test_option.py
+++ test/test_option.py
@@ -7,8 +7,57 @@ class OptionTestCase(unittest.TestCase):
         self.sut_present = Option(self.payload)
         self.sut_empty = Option(None)
 
-    def test_is_present_of_present_be_true(self):
-        self.assertTrue(self.sut_present.is_present())
+    @unittest.skip
+    def test_of(self):
+        None
 
-    def test_is_present_of_empty_be_false(self):
+    @unittest.skip
+    def test_of_nullable(self):
+        None
+
+    @unittest.skip
+    def test_empty(self):
+
+    def test_is_present(self):
+        self.assertTrue(self.sut_present.is_present())
         self.assertFalse(self.sut_empty.is_present())
+
+    def test_is_empty(self):
+        self.assertFalse(self.sut_present.is_empty())
+        self.assertTrue(self.sut_empty.is_empty())
+
+    @unittest.skip
+    def test_if_present(self):
+        None
+
+    @unittest.skip
+    def test_filter(self):
+        None
+
+    @unittest.skip
+    def test_map(self):
+        None
+
+    @unittest.skip
+    def test_flat_map(self):
+        None
+
+    @unittest.skip
+    def test_or_else(self):
+        None
+
+    @unittest.skip
+    def test_or_else_get(self):
+        None
+
+    @unittest.skip
+    def test_or_else_throw(self):
+        None
+
+    @unittest.skip
+    def test_or_none(self):
+        None
+
+    @unittest.skip
+    def test_stream(self):
+        None
