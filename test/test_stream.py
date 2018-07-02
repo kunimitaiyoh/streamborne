5362f93fab15bc76de75cf5555bcfd211bbcf804
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-07-02 22:05:10 +0900

    add tests for Stream.

--- test/test_stream.py
+++ test/test_stream.py
@@ -1,14 +1,26 @@
 import unittest
 from streamborne.stream import Stream
+from streamborne.option import Option
+
+mapper = lambda x: len(x)
+predicate = lambda x: len(x) == 5
 
 class StreamTestCase(unittest.TestCase):
+
     def setUp(self):
         self.items = ['fred', 'wilma', 'barney', 'betty', 'pebbles', 'bamm-bamm', 'dino']
 
+    def from_list(self):
+        return Stream.from_list(self.items)
+
     # region intermediate operations
+    def test_filter(self):
+        actual = self.from_list().filter(predicate).list()
+        expected = list(filter(predicate, self.items))
+        self.assertListEqual(actual, expected)
+
     def test_map(self):
-        mapper = lambda x: len(x)
-        actual = Stream.from_list(self.items).map(mapper).list()
+        actual = self.from_list().map(mapper).list()
         expected = list(map(mapper, self.items))
         self.assertListEqual(actual, expected)
     # endregion
